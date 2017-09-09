1e4f91d728efc834477a156f72cee24b231e163c
greatn8 <greatn8@users.noreply.github.com>
2017-09-09 18:30:47 +1000

    new update

--- beatsoupxmlparse.py
+++ beatsoupxmlparse.py
@@ -1,18 +1,41 @@
+#getting abn registered in a month and year & learning git
+
 from bs4 import BeautifulSoup
 import urllib2
 
 
+
 data = urllib2.urlopen('https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByRegistrationEvent?postcode=&state=&entityTypeCode=&month=8&year=2017&authenticationGuid=62301559-37d7-4505-8a1d-1c429ff09e63').read()
 
+#print data for testing
 print data
+#print type for testing
 print type(data)
 
+#makine string into soup and speicify its xml
 soup = BeautifulSoup(data, 'xml')
 print type(soup)
 
+#for each abn in xm document print abn
 for abn in soup.find_all('abn'):
 	print(abn.text)
 
 
-#
-#
+#function for getting details of each abn
+def getabndetails(abninstance):
+	abnindividual = str(abninstance)
+	abnsearch = urllib2.urlopen('https://abr.business.gov.au/abrxmlsearchRPC/AbrXmlSearch.asmx/SearchByABNv201408?searchString=' + abnindividual + '&includeHistoricalDetails=N&authenticationGuid=62301559-37d7-4505-8a1d-1c429ff09e63').read()
+	#print for testing
+	print abnsearch
+	#print type for testing
+	print type(abnsearch)
+
+	#makine string into soup and speicify its xml
+	soup1ABN = BeautifulSoup(abnsearch, 'xml')
+	print type(soup1ABN)
+
+	#for each abn in xm document print abn
+	#for abn in soup.find_all('abn'):
+	#	print(abn.text)
+#testing function with one ABN
+getabndetails(74172177893)
