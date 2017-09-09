072919ba2452f0577c5c03a79493fda03f8a9025
greatn8 <nathanshorter@gmail.com>
2017-09-09 16:42:56 +1000

    Inittial Commit

---
+++ beatsoupxmlparse.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+
+data = urllib2.urlopen('https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByRegistrationEvent?postcode=&state=&entityTypeCode=&month=8&year=2017&authenticationGuid=62301559-37d7-4505-8a1d-1c429ff09e63').read()
+
+print data
+print type(data)
+
+soup = BeautifulSoup(data, 'xml')
+print type(soup)
+
+for abn in soup.find_all('abn'):
+	print(abn.text)
+
+
+
